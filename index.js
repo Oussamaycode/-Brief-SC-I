623c4f57f1745a7817f21210abde70c0e983f04a
Oussamaycode <oussamalamlouki047@gmail.com>
2025-11-25 21:00:58 +0100

    Create rooms section and container divs

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const main = document.createElement('main');
 document.body.append(main);
 
+
 const unassigned = document.createElement('section');
 const unassignedheading = document.createElement('h2');
+
+
+const rooms = document.createElement('section');
+const roomscontainer = document.createElement('div');
+const roomscontainerleft = document.createElement('div');
+const roomscontainerright = document.createElement('div');
