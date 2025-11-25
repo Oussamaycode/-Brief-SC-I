038c344ba4d9a2887cd733c848fd57f8db715ecb
Oussamaycode <oussamalamlouki047@gmail.com>
2025-11-25 20:59:17 +0100

    Create unassigned section and heading

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const main = document.createElement('main');
 document.body.append(main);
+
+const unassigned = document.createElement('section');
+const unassignedheading = document.createElement('h2');
